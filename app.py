63ff496ba10d83191af6825aa88f714f119675f3
Irvingbarajas4 <94877353+Irvingbarajas4@users.noreply.github.com>
2021-12-15 19:45:22 -0600

    nuevo cambio

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager,UserMixin,login_user,login_required,logout_
 app = Flask(__name__)
 login_manager = LoginManager();
 login_manager.init_app(app);
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wxrfwirikeogvq:cb1c6db71147c0d4b47efd49e583571764f0dd3b8b7cb6eb4c5041c35dfe179e@ec2-184-73-25-2.compute-1.amazonaws.com:5432/d2cevncm3jqv3f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jckeilifwqadam:def9be2570de873b44f0e4541501aff4b926f8f3e992509254569ad6341e0ca1@ec2-184-73-25-2.compute-1.amazonaws.com:5432/dc6lpemuj67vep'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']='aloha'
 
